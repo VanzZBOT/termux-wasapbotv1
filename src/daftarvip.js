ba140fdce611b5d86f755fb1bcecebb2d47f1685
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-02-04 13:15:19 +0700

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,17 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*HARGA DAFTAR VIP :*
+-Rp. 5K > Akses Fitur ViP
+-Rp. 10K > Fitur VIP + Masukin Bot KeGrup Kalian!
+
+*JIKA INGIN DAFTAR VIP :*
+*Chat Owner BOT :*
+_https://bit.ly/2Yq4Efk atau ketik *${prefix}owner*
+
+*NOTE*
+
+*GRUP WHATSAPP BOT :*
+_https://chat.whatsapp.com/JQq5FsTzji8ElR7O8TJzOl `
+}
+exports.daftarvip = daftarvip
